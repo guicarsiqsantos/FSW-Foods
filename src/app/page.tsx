6ff767cac82e69f78daedecd4c26e902a5522cc1
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-07-16 11:50:15 -0300

    add promotion banner to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
 import Search from "@/components/search";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -12,6 +13,18 @@ const Home = () => {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promotion-Banner01.png"
+          alt="Ate 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vh"
+          quality={100}
+        />
       </div>
     </>
   );
