fbec7b1f11325ff7e432c9083023d63bea348c54
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-07-24 08:56:16 -0300

    add recommended restaurants page

--- src/components/restaurant-item.tsx
+++ src/components/restaurant-item.tsx
@@ -4,15 +4,17 @@ import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 interface RestaurantItemProps {
   restaurant: Restaurant;
+  className?: string;
 }
 
-const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
+const RestaurantItem = ({ restaurant, className }: RestaurantItemProps) => {
   return (
     <Link
-      className="min-w-[266px] max-w-[266px]"
+      className={cn("min-w-[266px] max-w-[266px]", className)}
       href={`/restaurants/${restaurant.id}`}
     >
       <div className="w-full space-y-3">
