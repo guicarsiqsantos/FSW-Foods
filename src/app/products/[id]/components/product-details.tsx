8b9d647e4f9558d44c4e4194144ecd7dfe49028d
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-07-24 08:36:47 -0300

    add restaurant page

--- src/app/products/[id]/components/product-details.tsx
+++ src/app/products/[id]/components/product-details.tsx
@@ -1,17 +1,12 @@
 "use client";
 
+import DeliveryInfo from "@/components/delivery-info";
 import DiscountBadge from "@/components/discount-badge";
 import ProductList from "@/components/product-list";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 import { calculateProductTotalPrice, formatCurrency } from "@/helpers/price";
 import { Prisma } from "@prisma/client";
-import {
-  BikeIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  TimerIcon,
-} from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -95,33 +90,7 @@ const ProductDetails = ({
       </div>
 
       <div className="px-5">
-        <Card className="mt-6 flex justify-around py-3">
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <span className="text-xs">Entrega</span>
-              <BikeIcon size={14} />
-            </div>
-
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className="text-xs font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </p>
-            ) : (
-              <p className="text-xs font-semibold">Grátis</p>
-            )}
-          </div>
-
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <span className="text-xs">Entrega</span>
-              <TimerIcon size={14} />
-            </div>
-
-            <p className="text-xs font-semibold">
-              {product.restaurant.deliveryTimeMinutes} min
-            </p>
-          </div>
-        </Card>
+        <DeliveryInfo restaurant={product.restaurant} />
       </div>
 
       <div className="mt-6 space-y-3 px-5">
--- src/components/restaurant-item.tsx
+++ src/components/restaurant-item.tsx
@@ -3,6 +3,7 @@ import { Restaurant } from "@prisma/client";
 import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface RestaurantItemProps {
   restaurant: Restaurant;
@@ -10,49 +11,54 @@ interface RestaurantItemProps {
 
 const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
   return (
-    <div className="min-w-[266px] max-w-[266px] space-y-3">
-      <div className="relative h-[136px] w-full">
-        <Image
-          src={restaurant.imageUrl}
-          alt={restaurant.name}
-          fill
-          className="rounded-lg object-cover"
-        />
+    <Link
+      className="min-w-[266px] max-w-[266px]"
+      href={`/restaurants/${restaurant.id}`}
+    >
+      <div className="w-full space-y-3">
+        <div className="relative h-[136px] w-full">
+          <Image
+            src={restaurant.imageUrl}
+            alt={restaurant.name}
+            fill
+            className="rounded-lg object-cover"
+          />
 
-        <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary bg-white px-2 py-[2px]">
-          <StarIcon size={12} className="fill-yellow-400 text-yellow-500" />
-          <span className="text-xs font-semibold">5.0</span>
-        </div>
-
-        <Button
-          size="icon"
-          className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
-        >
-          <HeartIcon size={16} className="fill-white" />
-        </Button>
-      </div>
-
-      <div>
-        <h3 className="text-sm font-semibold">{restaurant.name}</h3>
-        <div className="flex gap-3">
-          <div className="flex items-center gap-1">
-            <BikeIcon className="text-xs text-primary" size={14} />
-            <span className="text-xs text-muted-foreground">
-              {Number(restaurant.deliveryFee) === 0
-                ? "Entrega grátis"
-                : formatCurrency(Number(restaurant.deliveryFee))}
-            </span>
+          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary bg-white px-2 py-[2px]">
+            <StarIcon size={12} className="fill-yellow-400 text-yellow-500" />
+            <span className="text-xs font-semibold">5.0</span>
           </div>
 
-          <div className="flex items-center gap-1">
-            <TimerIcon className="text-xs text-primary" size={14} />
-            <span className="text-xs text-muted-foreground">
-              {restaurant.deliveryTimeMinutes} min
-            </span>
+          <Button
+            size="icon"
+            className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
+          >
+            <HeartIcon size={16} className="fill-white" />
+          </Button>
+        </div>
+
+        <div>
+          <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+          <div className="flex gap-3">
+            <div className="flex items-center gap-1">
+              <BikeIcon className="text-xs text-primary" size={14} />
+              <span className="text-xs text-muted-foreground">
+                {Number(restaurant.deliveryFee) === 0
+                  ? "Entrega grátis"
+                  : formatCurrency(Number(restaurant.deliveryFee))}
+              </span>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <TimerIcon className="text-xs text-primary" size={14} />
+              <span className="text-xs text-muted-foreground">
+                {restaurant.deliveryTimeMinutes} min
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
