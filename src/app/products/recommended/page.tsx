242343ad43dd01509fe316214f4be89fb266ac3b
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-07-24 09:45:41 -0300

    add recommended product page

---
+++ src/app/products/recommended/page.tsx
@@ -0,0 +1,41 @@
+import Header from "@/components/header";
+import ProductItem from "@/components/product-item";
+import { db } from "@/lib/prisma";
+
+const RecommendedProduct = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 20,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">Pedidos Recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecommendedProduct;
